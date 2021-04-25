e7357f901dcb759da07a7737e4770e582814b64c
shivanshi <shivanshi.saxena2@gmail.com>
2021-04-25 16:32:35 +0530

    Search Insert Position
    leetcode

---
+++ Search_insert_Position.cpp
@@ -0,0 +1,17 @@
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ If not, return the index where it would be if it were inserted in order. */
+ class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {       int i;
+        
+        for ( i = 0 ; i< nums.size() ; i++ )
+           
+           if (nums[i]>=target)
+            {
+                return i;
+            }
+          
+        return i;
+    }
+};
