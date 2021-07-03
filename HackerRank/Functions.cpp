95e9772a722c3d97074e3242abe272a83fe1dabc
shivanshi <shivanshi.saxena2@gmail.com>
2021-07-04 01:05:09 +0530

    Added Folders

--- HackerRank/Functions.cpp
+++ HackerRank/Functions.cpp
@@ -19,15 +19,15 @@ int max_of_four(int a, int b, int c, int d)
     {return b;}
     else if (c>=d)
     {return c;}
-    else 
+    else
     return d;
 }
-   
+
 int main() {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
     printf("%d", ans);
-    
+
     return 0;
 }
