db80444adf31cf23a4e96ab572767c45ddafd15b
shivanshi <shivanshi.saxena2@gmail.com>
2021-07-23 14:48:01 +0530

    added rec files

---
+++ BeginnersCode/static_var_recursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+  int fun (int n)
+  {
+    static int x=0;
+    if(n>0) {
+      x++;
+      return fun(n-1)+x;
+    }
+    return 0;
+  }
+
+  int main()
+  {
+    int r;
+    r = fun(5);
+    cout<<"sum "<<r<<endl;
+    return 0;
+  }
