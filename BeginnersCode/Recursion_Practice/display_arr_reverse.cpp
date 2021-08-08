44f434756103c1654e27fcd427ca909a3657628e
shivanshi <shivanshi.saxena2@gmail.com>
2021-08-08 16:20:30 +0530

    Display of an array

--- BeginnersCode/Recursion_Practice/display_arr_reverse.cpp
+++ BeginnersCode/Recursion_Practice/display_arr_reverse.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 using namespace std;
 
-//expectation- index se end tak sab print kar de
-//faith- index+1 se end tak karna janta hai
 int arraydisplay(int arr[],int index,int last)
 {
-    if(index >= last){
+    if(index < last){
         return 0;
-    } 
-  cout<<arr[index]<<" ";
-  arraydisplay(arr,index+1,last);
+    }
+     cout<<arr[index]<<" ";
+  arraydisplay(arr,index-1,last);
+
 }
 
 int main()
@@ -22,6 +21,6 @@ int main()
   for(int i=0;i<index;i++){
     cin>>arr[i];
   }
-  arraydisplay(arr,0,index);
+  arraydisplay(arr,index-1,0);
   return 0;
 }
