17bd9a6dbdab3d1799d3ed4bde819de8c8e5163b
shivanshi <shivanshi.saxena2@gmail.com>
2021-11-08 22:11:34 +0530

    hackerrank

---
+++ HackerRank/Problem_Solving/apple_oranges.cpp
@@ -0,0 +1,180 @@
+/*
+Sam's house has an apple tree and an orange tree that yield an abundance of fruit. Using the information given below, determine the number of apples and oranges that land on Sam's house.
+
+In the diagram below:
+
+The red region denotes the house, where  is the start point, and  is the endpoint. The apple tree is to the left of the house, and the orange tree is to its right.
+Assume the trees are located on a single point, where the apple tree is at point , and the orange tree is at point .
+When a fruit falls from its tree, it lands  units of distance from its tree of origin along the -axis. *A negative value of  means the fruit fell  units to the tree's left, and a positive value of  means it falls  units to the tree's right. *
+Apple and orange(2).png
+
+Given the value of  for  apples and  oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range )?
+
+For example, Sam's house is between  and . The apple tree is located at  and the orange at . There are  apples and  oranges. Apples are thrown  units distance from , and  units distance. Adding each apple distance to the position of the tree, they land at . Oranges land at . One apple and two oranges land in the inclusive range  so we print
+
+1
+2
+Function Description
+
+Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+countApplesAndOranges has the following parameter(s):
+
+s: integer, starting point of Sam's house location.
+t: integer, ending location of Sam's house location.
+a: integer, location of the Apple tree.
+b: integer, location of the Orange tree.
+apples: integer array, distances at which each apple falls from the tree.
+oranges: integer array, distances at which each orange falls from the tree.
+Input Format
+
+The first line contains two space-separated integers denoting the respective values of  and .
+The second line contains two space-separated integers denoting the respective values of  and .
+The third line contains two space-separated integers denoting the respective values of  and .
+The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
+The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
+
+Constraints
+
+Output Format
+
+Print two integers on two different lines:
+
+The first integer: the number of apples that fall on Sam's house.
+The second integer: the number of oranges that fall on Sam's house.
+Sample Input 0
+
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+Sample Output 0
+
+1
+1
+Explanation 0
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
+
+void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges) {
+
+}
+
+int main()
+{
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int s = stoi(first_multiple_input[0]);
+
+    int t = stoi(first_multiple_input[1]);
+
+    string second_multiple_input_temp;
+    getline(cin, second_multiple_input_temp);
+
+    vector<string> second_multiple_input = split(rtrim(second_multiple_input_temp));
+
+    int a = stoi(second_multiple_input[0]);
+
+    int b = stoi(second_multiple_input[1]);
+
+    string third_multiple_input_temp;
+    getline(cin, third_multiple_input_temp);
+
+    vector<string> third_multiple_input = split(rtrim(third_multiple_input_temp));
+
+    int m = stoi(third_multiple_input[0]);
+
+    int n = stoi(third_multiple_input[1]);
+
+    string apples_temp_temp;
+    getline(cin, apples_temp_temp);
+
+    vector<string> apples_temp = split(rtrim(apples_temp_temp));
+
+    vector<int> apples(m);
+
+    for (int i = 0; i < m; i++) {
+        int apples_item = stoi(apples_temp[i]);
+
+        apples[i] = apples_item;
+    }
+
+    string oranges_temp_temp;
+    getline(cin, oranges_temp_temp);
+
+    vector<string> oranges_temp = split(rtrim(oranges_temp_temp));
+
+    vector<int> oranges(n);
+
+    for (int i = 0; i < n; i++) {
+        int oranges_item = stoi(oranges_temp[i]);
+
+        oranges[i] = oranges_item;
+    }
+
+    countApplesAndOranges(s, t, a, b, apples, oranges);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
